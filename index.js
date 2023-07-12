cf3eefedf78347c0d2d279d2f0289e158d8ffce3
nguyenttnguyenparsley <116132802+nguyenttnguyenparsley@users.noreply.github.com>
2023-07-12 20:08:21 +0700

    feat: update server

--- index.js
+++ index.js
@@ -11,27 +11,49 @@ app.get("/test", (req, res) => {
   res.json({ status: 200 });
 });
 
+const device = {};
+
 // Connect Socket
 io.on("connection", (socket) => {
-  socket.on("room:join", (roomId) => {
+  socket.data = socket.handshake.query;
+
+  device[socket.id] = {
+    video: socket.data.video,
+  };
+
+  socket.on("room:join", async (roomId) => {
     const room = io.sockets.adapter.rooms.get(roomId);
+    const sockets = await io.in(roomId).fetchSockets();
 
     const otherUsers = [];
 
     if (room) {
       room.forEach((id) => {
-        otherUsers.push(id);
+        const s = sockets.find((s) => {
+          return s.id === id;
+        });
+
+        otherUsers.push({
+          id,
+          data: {
+            ...(s?.data || {}),
+            video: device[id]?.video,
+          },
+        });
       });
     }
-
     console.log(`🚀 [Socket - room:join] id: ${socket.id} - roomId: ${roomId}`);
 
     socket.join(roomId);
     socket.emit("room:all-other-users", otherUsers);
   });
 
-  socket.on("peer connection request", ({ userIdToCall, sdp }) => {
-    io.to(userIdToCall).emit("peer:offer", { sdp, callerId: socket.id });
+  socket.on("peer:request", ({ userIdToCall, sdp }) => {
+    io.to(userIdToCall).emit("peer:offer", {
+      sdp,
+      callerId: socket.id,
+      data: socket?.data || {},
+    });
   });
 
   socket.on("peer:answer", ({ userToAnswerTo, sdp }) => {
@@ -45,10 +67,19 @@ io.on("connection", (socket) => {
     io.to(target).emit("peer:ice-candidate", { candidate, from: socket.id });
   });
 
+  socket.on("peer:hide-video", ({ id, roomId }) => {
+    device[id] = { video: false };
+    socket.to(roomId).emit("peer:hide-video", id);
+  });
+
+  socket.on("peer:show-video", ({ id, roomId }) => {
+    device[id] = { video: true };
+    socket.to(roomId).emit("peer:show-video", id);
+  });
+
   socket.on("disconnecting", () => {
-    console.log("socket.rooms", socket.rooms);
     socket.rooms.forEach((room) => {
-      socket.to(room).emit("user:leave", socket.id);
+      socket.to(room).emit("peer:leave", socket.id);
     });
   });
 });
